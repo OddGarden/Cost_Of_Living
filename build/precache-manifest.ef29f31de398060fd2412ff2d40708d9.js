10cc466071c60e9dc503588fc4c4dc91a065b2ba
OddGarden <oddgarden@outlook.com>
2019-06-09 17:57:25 -0700

    initial cost of living version

---
+++ build/precache-manifest.ef29f31de398060fd2412ff2d40708d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49863aec023f75a0edb1",
+    "url": "/project-munyii18386/static/js/runtime~main.49863aec.js"
+  },
+  {
+    "revision": "7ade5eabc60bee5d18e1",
+    "url": "/project-munyii18386/static/js/main.7ade5eab.chunk.js"
+  },
+  {
+    "revision": "82c83673391f1b9b3007",
+    "url": "/project-munyii18386/static/js/1.82c83673.chunk.js"
+  },
+  {
+    "revision": "7ade5eabc60bee5d18e1",
+    "url": "/project-munyii18386/static/css/main.a3cc089f.chunk.css"
+  },
+  {
+    "revision": "82c83673391f1b9b3007",
+    "url": "/project-munyii18386/static/css/1.71f9bcf7.chunk.css"
+  },
+  {
+    "revision": "80b0cca0ffea604ee12e938e7e55e360",
+    "url": "/project-munyii18386/index.html"
+  }
+];
