b37fda22b03e3448bd7f83d2864e2a197f2a1328
OddGarden <oddgarden@outlook.com>
2019-06-09 19:28:46 -0700

    Updates

---
+++ precache-manifest.57bd2220bd2d519144aa794efecb6de2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "52f707f3bec39261d955",
+    "url": "/CostOfLiving/static/js/runtime~main.52f707f3.js"
+  },
+  {
+    "revision": "25356af9cf87dad66e15",
+    "url": "/CostOfLiving/static/js/main.25356af9.chunk.js"
+  },
+  {
+    "revision": "30dae98024a84114058f",
+    "url": "/CostOfLiving/static/js/1.30dae980.chunk.js"
+  },
+  {
+    "revision": "25356af9cf87dad66e15",
+    "url": "/CostOfLiving/static/css/main.0bde36a0.chunk.css"
+  },
+  {
+    "revision": "30dae98024a84114058f",
+    "url": "/CostOfLiving/static/css/1.01eee7d0.chunk.css"
+  },
+  {
+    "revision": "3b8279c41d537b90fd56460ebd9de260",
+    "url": "/CostOfLiving/index.html"
+  }
+];
